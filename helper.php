ed34feef5846ddd6a5b745c442e62a93cd8c4408
Asika <asika@asikart.com>
2012-04-11 14:53:54 +0800

    files

---
+++ helper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_articles_latest
+ * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+abstract class modExampleHelper
+{
+	public static function getList(&$params)
+	{
+		$list = null ;
+		return $list ;
+	}
+}
---
+++ tmpl/default.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @version		$Id: default.php 22338 2011-11-04 17:24:53Z github_bot $
+ * @package		Joomla.Site
+ * @subpackage	mod_footer
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+?>
+<div>Mod Example</div>
